f7cef062efb50aea4be25ec2f39ab6916befa88a
inso <wrfsh0@gmail.com>
2013-09-30 23:27:44 +0700

    Extracted system video dependecies into efi/i_video.c

---
+++ linuxdoom-1.10/efi/i_video.c
@@ -0,0 +1,46 @@
+#include <i_video.h>
+
+// Called by D_DoomMain,
+// determines the hardware configuration
+// and sets up the video mode
+void I_InitGraphics (void)
+{
+
+}
+
+void I_ShutdownGraphics(void)
+{
+
+}
+
+// Takes full 8 bit values.
+void I_SetPalette (byte* palette)
+{
+
+}
+
+void I_UpdateNoBlit (void)
+{
+
+}
+
+void I_FinishUpdate (void)
+{
+
+}
+
+void I_ReadScreen (byte* scr)
+{
+
+}
+
+void I_StartFrame (void)
+{
+
+}
+
+void I_StartTic (void)
+{
+
+}
+
